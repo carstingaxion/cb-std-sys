302654fe7eb32db413c515204e9e50cb5f3188a5
Carsten Bach <mail@carsten-bach.de>
2012-10-18 11:59:34 +0200

    use this to overwrite constants from Parentheme

---
+++ wp-content/themes/YOUR-CHILDTHEME-NAME/inc/constants.php
@@ -0,0 +1,21 @@
+<?php
+    /**
+     *    Redefine Constants
+     *    
+     *    @since    0.2.1
+     */
+    define( 'CBSTDSYS_SUPER_ADMIN_EMAIL', 'mail@carsten-bach.de' ); 
+    
+    
+    
+    /**
+     *  Define Dimensions for Custom Header Image
+     *  
+     *  @since  0.1
+     */
+#    define( 'HEADER_TEXTCOLOR', '' );
+    define( 'HEADER_IMAGE', '%s/images/headers/path.jpg' );                                      
+    define( 'HEADER_IMAGE_WIDTH', 954 );
+    define( 'HEADER_IMAGE_HEIGHT', 517 );
+#    define( 'NO_HEADER_TEXT', true );
+?>
