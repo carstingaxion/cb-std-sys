287a8ace9ab3c80261bf7ed59bc61a2cf19ea867
Carsten Bach <mail@carsten-bach.de>
2012-10-22 08:33:35 +0200

    removed WP functions

--- wp-content/maintenance.php
+++ wp-content/maintenance.php
@@ -12,7 +12,7 @@ header( 'Retry-After: 600' );
 $wp_themed_error_body_class  = 'maintenance-mode';
 $wp_themed_error_title_tag   = 'Wartungsarbeiten';  
 $wp_themed_error_title       = 'Wartungsarbeiten';
-$wp_themed_error_home_link   = '<a href="'. WP_SITEURL .'" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) .' Startseite" rel="home">' . get_option( 'blogname' ) .' Startseite</a>'; 
+$wp_themed_error_home_link   = '<a href="'. WP_SITEURL .'" title="Startseite" rel="home">Startseite</a>'; 
 $wp_themed_error_content     = '<p>Wir arbeiten für Sie gerade an der Verbesserung dieser Seite.</p><p>Bitte entschuldigen Sie die Unterbrechung. </p><p>Wir sind in Kürze zurück.</p>';
 
 include_once getenv('DOCUMENT_ROOT').'/wp-content/wp_themed_error.php';
